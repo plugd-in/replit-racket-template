b89f5f37583f8b05c5212a7827f34379e5c6955d
ReyBontje <>
2021-10-21 05:07:09 +0000

    Minor changes.

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
 if [ ! -d ~/racket ]; then
-  wget 'https://mirror.csclub.uwaterloo.ca/racket/racket-installers/8.2/racket-8.2-x86_64-linux-cs.sh'
-  sh ./racket-8.0-x86_64-linux-cs.sh --in-place --dest ~/racket
+  [ ! -f "./racket-8.2-x86_64-linux-cs.sh" ] && wget 'https://mirror.csclub.uwaterloo.ca/racket/racket-installers/8.2/racket-8.2-x86_64-linux-cs.sh'
+  sh ./racket-8.*-x86_64-linux-cs.sh --in-place --dest ~/racket
   echo 'PATH="$PATH:~/racket/bin"' >> ~/.bashrc
 fi;
 clear
 echo "In order to start DrRacket, type the command 'drracket'."
+echo "Make sure to save files to $(pwd)"
 bash -i
